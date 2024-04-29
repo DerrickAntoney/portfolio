38a4b2631a10e6b10d1ecb36809197657c95809d
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-29 14:43:41 +0300

    Revert changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
       setSuccess(false);
   
       emailjs
-        .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
-          publicKey: KEY,
+        .sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
         })
         .then(
           (result) => {
