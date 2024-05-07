11b36bdfc6146fee8387443a05b79cb4f7424fec
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-05-08 00:13:08 +0300

    Button and footer updates

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-black font-medium text-lg mt-5'>
-      <layout className='flex items-center justify-center py-2'>
-        <span>Copyright</span>
-        <span>&copy;</span>
-        <span>Derrick {new Date().getFullYear()}</span>
+      <layout className='flex flex-col items-center justify-center py-6'>
+        <div>
+          <span>{new Date().getFullYear()}</span>
+          <span>&copy;</span>
+          <span>All rights reserved</span>
+        </div>
+        <div className='flex items-center'>Made with  <img src="/love.png" alt="Love Icon" className='h-6 w-5'/>  by Derrick</div>
       </layout>
     </footer>
   )
